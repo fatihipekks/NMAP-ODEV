a2b3fac2f4b7996e4a40235e4507ca18d8c01093
fatihipekks <74711684+fatihipekks@users.noreply.github.com>
2022-04-22 21:15:30 +0300

    C# Dili ile Nmap Uygulaması Kullanmak

---
+++ webNmap/webNmap/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace odevNmap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a;
+
+            Console.Write("Lütfen İP Giriniz: ");
+            a = Console.ReadLine();
+            
+            //Process Sınıfı Oluşturduk.
+            Process process = new Process();
+        
+            process.StartInfo.FileName = "nmap.exe";
+            process.StartInfo.Arguments = "nmap -T4 -A -v " + a;
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Maximized;
+            process.Start();
+            
+            // Process'i bitirir.
+            process.WaitForExit();
+
+        }
+    }
+}
